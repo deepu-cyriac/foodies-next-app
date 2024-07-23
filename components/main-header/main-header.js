21e1aed5acb171d06bb5b1504b37733b22c836c9
Deepu Cyriac <deepucvk123@gmail.com>
2024-07-23 08:35:57 +0530

    Community page & header - nav changes

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,8 +4,11 @@ import logoImg from "@/assets/logo.png";
 
 import classes from './main-header.module.css';
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+
   return (
     <>
     <MainHeaderBackground />
@@ -18,10 +21,10 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Meals</Link>
+            <NavLink href="/meals">Browse Meals</NavLink>
           </li>
           <li>
-            <Link href="/community">Community</Link>
+            <NavLink href="/community">Foodies Community</NavLink>
           </li>
         </ul>
       </nav>
