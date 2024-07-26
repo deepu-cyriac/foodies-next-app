427bdedb405492274ae2ba550a48c2c7c68753c9
Deepu Cyriac <deepucvk123@gmail.com>
2024-07-26 08:53:03 +0530

    Added metadata & dynamic metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,12 +1,24 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import { getMeal } from '@/lib/meals';
+import { getMeal } from "@/lib/meals";
 
-import classes from './page.module.css';
-import { notFound } from 'next/navigation';
+import classes from "./page.module.css";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
 
 export default function MealDetailsPage({ params }) {
-
   const meal = getMeal(params.mealSlug);
 
   if (!meal) {
@@ -14,13 +26,13 @@ export default function MealDetailsPage({ params }) {
     notFound();
   }
 
-  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill/>
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
@@ -31,9 +43,12 @@ export default function MealDetailsPage({ params }) {
         </div>
       </header>
       <main>
-        <p className={classes.instructions} dangerouslySetInnerHTML={{
-          __html: meal.instructions
-        }}></p>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
       </main>
     </>
   );
